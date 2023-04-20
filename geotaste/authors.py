38e19eca3ce165677a9ff5af4a284d08ca01bea8
Ryan Heuser <ryan.heuser@gmail.com>
2023-04-20 14:46:44 -0400

    adding author data

---
+++ geotaste/authors.py
@@ -0,0 +1,24 @@
+from .imports import *
+
+
+################
+#### BOOKS #####
+################
+
+def get_author_id(x): return ''.join(y for y in x if y.isalpha() or y==' ').lower().replace(' ','-')
+@cache
+def get_authors_df():
+    df=get_urlpath_df('authors').fillna('').drop('gender',1)
+    df.columns = [f'author_{str(col).lower().replace(" ","_")}' for col in df.columns]
+    df['author_id']=df['author_sort_name'].apply(get_author_id)
+    
+    df['author_is_expat'] = df['author_nationality'].apply(lambda x: 'France' not in x)
+    # df['has_wikipedia'] = df['wikipedia_url']!=''
+    # df['has_viaf'] = df['viaf_url']!=''
+    df['author_birth_decade'] = [str(x)[:3]+'0s' if x else '' for x in df['author_birth_year']]
+
+    from .people import parse_generation
+    df['author_generation'] = df['author_birth_year'].apply(parse_generation)
+    
+    return df.set_index('author_id')
+
--- geotaste/books.py
+++ geotaste/books.py
@@ -7,8 +7,23 @@ from .imports import *
 
 def get_book_id(x): return x.split('/books/',1)[1][:-1] if '/books/' in x else ''
 @cache
-def get_books_df(): 
+def get_books_df(with_author_data=True): 
     df=get_urlpath_df('books').fillna('')
     df['book_id']=df.uri.apply(get_book_id)
+    df['book_dec'] = [str(x)[:3]+'0s' if x else '' for x in df['year'].fillna('')]
+    df['first_author'] = [str(x).split(';')[0] for x in df.author.fillna('')]
+    
+    
+    ## merge with authors?
+    if with_author_data:
+        df_au = get_authors_df()
+        df = df.merge(
+            df_au, 
+            left_on='first_author', 
+            right_on='author_sort_name', 
+            how='left', 
+            suffixes=('', '_au')
+        )
+
     return df.set_index('book_id')
 
