9df31dd5943c7e538337fb5fd22c66c2f9a5d8cc
Ryan Heuser <ryan.heuser@gmail.com>
2023-05-12 08:39:52 -0400

    toward new

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@
 ## Constants
 TITLE = 'Geography of Taste'
 TABLE_TEXT_COLOR='white'
+COLORABLE_COLS = ['arrond_id', 'gender', 'is_expat', 'nation']
 
 ## Sys imports
 from datetime import datetime as dt
@@ -17,6 +18,7 @@ from geotaste import *
 ## Non-sys imports
 import dash
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -25,7 +27,7 @@ import numpy as np
 import pandas as pd
 from flask_caching import Cache
 import diskcache
-from dash import Dash, dcc, html, Input, Output, DiskcacheManager, dash_table, State
+from dash import Dash, dcc, html, Input, Output, DiskcacheManager, dash_table
 import pandas_dash
 
 #############
@@ -100,127 +102,84 @@ def get_filtered_data_members(): return get_total_data_members()
 # LAYOUT #
 ##########
 
+def get_dropdown(series, id=None, sort_alpha=False, default=None, default_all=None, multi=True, prefix='dropdown', **kwargs):
+    if id is None and hasattr(series,'name'): id=series.name
+    assert id is not None
 
-
-def DashMembersDataTable(df=None):
-    df = get_total_data_members() if df is None else df
-    ocols = ['name','title','gender','birth_year','death_year','nationalities','street_address', 'start_date', 'end_date']
-    ddt, ddt_cols = df[ocols].dash.to_dash_table(
-        # column_properties={"country": {"presentation": "markdown"}}
-    )
-    return dash_table.DataTable(
-        data=ddt,
-        columns=ddt_cols,
-        sort_action="native",
-        sort_mode="multi",
-        filter_action="native",
-        merge_duplicate_headers=True,
-        # virtualization=True,
-        # style_cell={'color':TABLE_TEXT_COLOR},
-        # style_table={'height':300},
-        # style_as_list_view=True,
-        # style_data_conditional=[
-        #     {
-        #         'if': {'row_index': 'odd'},
-        #         'backgroundColor': 'rgba(255, 255, 255, .1)',
-        #     }
-        # ],
-        
-    )
-
-
-
-def DashTab(children=[], id='', label=''):
-    return dcc.Tab(
-        children,
-        id,
-        label,
-    )
-
-def DashMembersTab():
-    df = get_total_data_members()
+    # get options
+    opts = list(series.value_counts().index) if not sort_alpha else list(sorted(set(series)))
+    opts = [str(x) for x in opts]
+    opts_ld = [dict(label=x if x!='' else '(empty)', value=x) for x in opts]
     
-    # is_expat = dcc.RadioItems(
-    #     options=df.dash.to_options('is_expat'),
-    #     value='True',
-    #     id='radio-is_expat'
-    # )
-    is_expat = dcc.Dropdown(
-        options=['True','False'],
-        value=['True','False'],
-        id='dropdown-is_expat',
-        multi=True
+    o_id = f'{str(prefix)+"-" if prefix else ""}{id if id else series.name}'
+    print(id,'...',o_id)
+    drop = dcc.Dropdown(
+        options=opts_ld,
+        value=(default if default is not None else (opts if default_all else [])),
+        id=o_id,
+        multi=multi
     )
-    
-    all_nations = [k for k,v in Counter([nat for nations in df.nationalities for nat in nations.split(';')]).most_common()]
-    nations = dcc.Dropdown(
-        options=all_nations,
-        value=[],
-        id='dropdown-nation',
-        multi=True,
-    )
+    drop.id_orig = id
+    return drop
 
-    genders_l = list(df.gender.value_counts().index)
-    genders = dcc.Dropdown(
-        options=genders_l,
-        value=genders_l,
-        id='dropdown-gender',
-        multi=True,
-    )
-    
-    
-    
-    return DashTab(
-        children=[
-            html.Label('The library member is an expat (i.e. not French).'),
-            is_expat,
-            
-            html.Label('Nationality of member'), nations,
+def get_labeled_dropdown(*args, label='', **kwargs):
+    dropdown = get_dropdown(*args, **kwargs)
+    if not label: label = dropdown.id_orig.replace('_',' ').title() + ('?' if dropdown.id_orig.startswith('is_') else '')
+    return dbc.Row([
+        html.Label(label),
+        dropdown,
+        html.Hr()
+    ])
 
-            html.Label('Gender of member'), genders
-            
-        ],
-        id='members_tab',
-        label='Members',
+
+
+
+
+
+def get_mantine(children=[]): 
+    return dmc.MantineProvider(
+        theme={
+            "fontFamily": "'Inter', sans-serif",
+            "primaryColor": "indigo",
+            "components": {
+                "Button": {"styles": {"root": {"fontWeight": 400}}},
+                "Alert": {"styles": {"title": {"fontWeight": 500}}},
+                "AvatarGroup": {"styles": {"truncated": {"fontWeight": 500}}},
+            },
+        },
+        inherit=True,
+        withGlobalStyles=True,
+        withNormalizeCSS=True,
+        children=children
     )
 
-def DashBooksTab():
-    df = get_books_df().fillna('')
-    authors = list(df['author'].value_counts().index)
+burger = dmc.Burger(id='burger')
+
+
+layout = dbc.Container(
+    dbc.Row(
+    [
+    dbc.Col([
+        html.H1(TITLE),
+        get_labeled_dropdown(COLORABLE_COLS, id='color_cols', sort_alpha=True),
+        get_labeled_dropdown(get_members_df().gender),
+        get_labeled_dropdown(get_members_df().is_expat, multi=False),
+
+    ], width=3, id='layout-col-left'),
     
-    content = [
-        dcc.Dropdown(
-            options=authors,
-            value='Woolf, Virginia',
-            id='dropdown-author'
-        )
-    ]
-    
-    return DashTab(
-        content,
-        id='books_tab',
-        label='Books',
-    )
+    dbc.Col([
+        dbc.Row([
+            dcc.Graph(id='members-map'),
+            html.Div(id='members-table')
+        ]),
+    ], width=9, id='layout-col-right'),
 
-def DashEventsTab():
-    return DashTab(
-        id='events_tab',
-        label='Events',
-    )
+]), id='layout-container')
 
 
-def DashTabLayout():
-    return dcc.Tabs(
-            [
-                DashMembersTab(),
-                DashBooksTab(),
-                DashEventsTab()
-            ]
-        )
+app.layout = get_mantine(layout)
 
 
-
-    
 # Update Map Graph based on date-picker, selected data on histogram and location dropdown
 @app.callback(
     [
@@ -229,11 +188,10 @@ def DashTabLayout():
     ],
     [
         Input('dropdown-is_expat', 'value'),
-        Input('dropdown-nation', 'value'),
         Input('dropdown-gender', 'value'),
     ]
 )
-def DashMembersMap(is_expat=None, nation=None, gender=None, color_by='arrond_id'):
+def DashMembersMap(is_expat=None, gender=None, color_by='arrond_id'):
     dff = get_filtered_data_members().sample(frac=1)
     
     if is_expat:
@@ -284,105 +242,6 @@ def DashMembersMap(is_expat=None, nation=None, gender=None, color_by='arrond_id'
 
 
 
-def DashOffLayout():
-    return html.Div([
-        dbc.Offcanvas(
-            DashLeftLayout(),
-            id="offcanvas",
-            title="Title????",
-            is_open=False,
-        )
-    ])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.callback(
-    Output("offcanvas", "is_open"),
-    Input("open-offcanvas", "n_clicks"),
-    [State("offcanvas", "is_open")],
-)
-def toggle_offcanvas(n1, is_open):
-    if n1:
-        return not is_open
-    return is_open
-
-def DashLeftLayout():
-    return dbc.Col(
-        children=[
-            html.H1(TITLE),
-            DashTabLayout()
-        ],
-        width=4,
-        id='layout-col-left'
-    )
-
-
-def DashMemberMapTab():
-    return dbc.Tab(
-        dbc.Row([
-            dcc.Graph(id='members-map'),
-            html.Div(id='members-table')
-        ]),
-        id='members_map_tab',
-        label='Map members'
-    )
-
-def DashEventsMapTab():
-    return dbc.Tab(
-        dcc.Graph(id='events-map'),
-        id='events_map_tab',
-        label='Map events'
-    ) 
-
-def DashRightLayout():
-    return dbc.Col(
-        dbc.Tabs([
-            DashMemberMapTab(),
-            DashEventsMapTab()
-        ]),
-        width=8,
-        id='layout-col-right'
-    )
-
-def DashLayout():
-    return dbc.Container([
-        dbc.Row([
-            dbc.Col('Hello'),
-            dbc.Col([
-                dbc.Button("Open Offcanvas", id="open-offcanvas", n_clicks=0)
-            ],
-            style={'text-align':'right'}
-            )
-        ]),
-        dbc.Row([
-            DashRightLayout()
-        ], id='layout-container-row1'),
-    ], id='layout-container')
-   
-
-#   
-
-app.layout = DashLayout()
 
 if __name__ == "__main__":
     app.run(
@@ -396,3 +255,257 @@ if __name__ == "__main__":
         reloader_interval=1,
         reloader_type='watchdog'
     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def DashMembersDataTable(df=None):
+#     df = get_total_data_members() if df is None else df
+#     ocols = ['name','title','gender','birth_year','death_year','nationalities','street_address', 'start_date', 'end_date']
+#     ddt, ddt_cols = df[ocols].dash.to_dash_table(
+#         # column_properties={"country": {"presentation": "markdown"}}
+#     )
+#     return dash_table.DataTable(
+#         data=ddt,
+#         columns=ddt_cols,
+#         sort_action="native",
+#         sort_mode="multi",
+#         filter_action="native",
+#         merge_duplicate_headers=True,
+#         # virtualization=True,
+#         # style_cell={'color':TABLE_TEXT_COLOR},
+#         # style_table={'height':300},
+#         # style_as_list_view=True,
+#         # style_data_conditional=[
+#         #     {
+#         #         'if': {'row_index': 'odd'},
+#         #         'backgroundColor': 'rgba(255, 255, 255, .1)',
+#         #     }
+#         # ],
+        
+#     )
+
+
+
+# def DashTab(children=[], id='', label=''):
+#     return dcc.Tab(
+#         children,
+#         id,
+#         label,
+#     )
+
+# def DashMembersTab():
+#     df = get_total_data_members()
+    
+#     # is_expat = dcc.RadioItems(
+#     #     options=df.dash.to_options('is_expat'),
+#     #     value='True',
+#     #     id='radio-is_expat'
+#     # )
+#     is_expat = dcc.Dropdown(
+#         options=['True','False'],
+#         value=['True','False'],
+#         id='dropdown-is_expat',
+#         multi=True
+#     )
+    
+#     all_nations = [k for k,v in Counter([nat for nations in df.nationalities for nat in nations.split(';')]).most_common()]
+#     nations = dcc.Dropdown(
+#         options=all_nations,
+#         value=[],
+#         id='dropdown-nation',
+#         multi=True,
+#     )
+
+#     genders_l = list(df.gender.value_counts().index)
+#     genders = dcc.Dropdown(
+#         options=genders_l,
+#         value=genders_l,
+#         id='dropdown-gender',
+#         multi=True,
+#     )
+    
+    
+    
+#     return DashTab(
+#         children=[
+#             html.Label('The library member is an expat (i.e. not French).'),
+#             is_expat,
+            
+#             html.Label('Nationality of member'), nations,
+
+#             html.Label('Gender of member'), genders
+            
+#         ],
+#         id='members_tab',
+#         label='Filter members',
+#     )
+
+# def DashBooksTab():
+#     df = get_books_df().fillna('')
+#     authors = list(df['author'].value_counts().index)
+    
+#     content = [
+#         dcc.Dropdown(
+#             options=authors,
+#             value='Woolf, Virginia',
+#             id='dropdown-author'
+#         )
+#     ]
+    
+#     return DashTab(
+#         content,
+#         id='books_tab',
+#         label='Books',
+#     )
+
+# def DashEventsTab():
+#     return DashTab(
+#         id='events_tab',
+#         label='Events',
+#     )
+
+
+# def DashTabLayout():
+#     return dcc.Tabs(
+#             [
+#                 DashMembersTab(),
+#                 DashBooksTab(),
+#                 DashEventsTab()
+#             ]
+#         )
+
+
+
+    
+# # Update Map Graph based on date-picker, selected data on histogram and location dropdown
+# @app.callback(
+#     [
+#         Output("members-map", "figure"),
+#         Output("members-table", "children"),
+#     ],
+#     [
+#         Input('dropdown-is_expat', 'value'),
+#         Input('dropdown-nation', 'value'),
+#         Input('dropdown-gender', 'value'),
+#     ]
+# )
+# def DashMembersMap(is_expat=None, nation=None, gender=None, color_by='arrond_id'):
+#     dff = get_filtered_data_members().sample(frac=1)
+    
+#     if is_expat:
+#         dff = dff[dff.is_expat.apply(str).isin(is_expat)]
+    
+#     if gender:
+#         dff = dff[dff.gender.apply(str).isin(gender)]
+        
+#     if nation:
+#         dff = dff[dff.nation.apply(lambda x: bool(set(x.split(';')) & set(nation)))]
+        
+#     fig_choro = px.choropleth_mapbox(
+#         dff,
+#         geojson=get_geojson_arrondissement(),
+#         locations='arrond_id', 
+#         color='arrond_id',
+#         center=dict(lat=latlon_SCO[0], lon=latlon_SCO[1]),
+#         zoom=12,
+#         opacity=.1
+#     )
+#     # fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        
+
+#     fig = px.scatter_mapbox(
+#         dff, 
+#         lat="lat", 
+#         lon="lon", 
+#         center=dict(lat=latlon_SCO[0], lon=latlon_SCO[1]),
+#         # hover_name="member_id", 
+#         color='is_expat',
+#         # hover_data=["State", "Population"],
+#         # color_discrete_sequence=["fuchsia"], 
+#         zoom=12, 
+#         # size=10
+#         # marker=dict(size=100),
+#         # height=300
+#     )
+#     fig.update_layout(
+#         mapbox=dict(
+#             style="stamen-toner",
+#         ),
+#     )
+#     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    
+#     fig3 = go.Figure(data=fig.data + fig_choro.data, layout = fig.layout)
+    
+#     return fig3, DashMembersDataTable()
+
+
+
+
+
+
+
+# def DashLeftLayout():
+#     return dbc.Col(
+#         children=[
+#             html.H1(TITLE),
+#             DashTabLayout()
+#         ],
+#         width=4,
+#         id='layout-col-left'
+#     )
+
+
+# def DashMemberMapTab():
+#     return dbc.Tab(
+#         dbc.Row([
+#             dcc.Graph(id='members-map'),
+#             html.Div(id='members-table')
+#         ]),
+#         id='members_map_tab',
+#         label='Map members'
+#     )
+
+# def DashEventsMapTab():
+#     return dbc.Tab(
+#         dcc.Graph(id='events-map'),
+#         id='events_map_tab',
+#         label='Map events'
+#     ) 
+
+# def DashRightLayout():
+#     return dbc.Col(
+#         dbc.Tabs([
+#             DashMemberMapTab(),
+#             DashEventsMapTab()
+#         ]),
+#         width=8,
+#         id='layout-col-right'
+#     )
+
+# def DashLayout():
+#     return dbc.Container([
+#         dbc.Row([
+#             DashLeftLayout(), 
+#             DashRightLayout()
+#         ], id='layout-container-row1'),
+#     ], id='layout-container')
+   
+
+# #   
+
