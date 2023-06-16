0d4ba0e1429a06c4ab91e05ec082a2f3f45902c0
Ryan Heuser <ryan.heuser@gmail.com>
2023-06-16 04:43:36 -0400

    datasets now pull from URL

--- geotaste/imports.py
+++ geotaste/imports.py
@@ -3,9 +3,7 @@ import os
 import pandas as pd
 pd.options.mode.chained_assignment = None  # default='warn'
 from functools import cached_property, lru_cache
-# cache = lru_cache(maxsize=None)
-# from diskcache import Cache
-from joblib import Memory
+cache = lru_cache(maxsize=None)
 
 
 #########
@@ -14,20 +12,35 @@ from joblib import Memory
 
 PATH_HERE = os.path.abspath(os.path.dirname(__file__))
 PATH_REPO = os.path.dirname(PATH_HERE)
-PATH_DATA = os.path.join(PATH_REPO,'data')
-PATH_CACHE = os.path.join(PATH_DATA,'cache')
-# cache = Cache(PATH_CACHE)
-cache = Memory(PATH_CACHE, verbose=1).cache
+# PATH_DATA = os.path.join(PATH_REPO,'data')
+PATH_DATA = os.path.expanduser('~/geotaste_data')
+
+# Urls
+URLS=dict(
+    books='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.3-beta/books.csv',
+    members='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.3-beta/members.csv',
+    events='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.3-beta/events.csv',
+    dwellings='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.3-beta/dwellings.csv',
+    creators='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.2/creators.csv',
+    geojson_arrond='https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/arrondissements/exports/geojson?lang=en&timezone=Europe%2FParis'
+)
 
 # Paths
 PATHS=dict(
     here=PATH_HERE,
     data=PATH_DATA,
-    members = os.path.join(PATH_DATA,'1.3-beta','members.csv'),
-    books = os.path.join(PATH_DATA,'1.3-beta','books.csv'),
-    events = os.path.join(PATH_DATA,'1.3-beta','events.csv'),
-    dwellings = os.path.join(PATH_DATA,'1.3-beta','dwellings.csv'),
-    creators = os.path.join(PATH_DATA,'1.2','creators.csv'),
+    
+    members = os.path.join(PATH_DATA,'members.csv'),
+    books = os.path.join(PATH_DATA,'books.csv'),
+    events = os.path.join(PATH_DATA,'events.csv'),
+    dwellings = os.path.join(PATH_DATA,'dwellings.csv'),
+    creators = os.path.join(PATH_DATA,'creators.csv'),
+    
+    # members = os.path.join(PATH_DATA,'1.3-beta','members.csv'),
+    # books = os.path.join(PATH_DATA,'1.3-beta','books.csv'),
+    # events = os.path.join(PATH_DATA,'1.3-beta','events.csv'),
+    # dwellings = os.path.join(PATH_DATA,'1.3-beta','dwellings.csv'),
+    # creators = os.path.join(PATH_DATA,'1.2','creators.csv'),
 )
 
 ### other vars
@@ -41,14 +54,4 @@ DWELLING_ID_SEP='; '
 ####
 
 
-from .utils import *
 from .datasets import *
-# from .arronds import *
-# from .authors import *
-# from .books import *
-# from .choices import *
-# from .combined import *
-# from .dwellings import *
-# from .events import *
-# from .maps import *
-# from .people import *
