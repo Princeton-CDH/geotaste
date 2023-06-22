5ffc8bc9b9ccf7155460df0dad0f46da0a4e403e
Ryan Heuser <ryan.heuser@gmail.com>
2023-06-22 19:44:43 -0400

    js clientside solution to updating window size

--- geotaste/app/app.py
+++ geotaste/app/app.py
@@ -14,6 +14,6 @@ def run():
     # move to here
     # app.run(8052, host='0.0.0.0')
     print('booting')
-    app.run(8053, debug=True, dev_tools_ui=False, dev_tools_hot_reload=True)
+    app.run(8053, debug=True)#, dev_tools_ui=False, dev_tools_hot_reload=True)
 
 if __name__=='__main__': run()
