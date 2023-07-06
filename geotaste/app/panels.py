fd24cfada2c57f5092960f6d2d28eff6d28b8d74
Ryan Heuser <ryan.heuser@gmail.com>
2023-07-06 10:44:52 -0400

    within filters working

--- geotaste/app/panels.py
+++ geotaste/app/panels.py
@@ -5,7 +5,14 @@ class FilterPanel(FilterComponent):
     @cached_property
     def subcomponents(self):
         return []  # subclass this
+    @cached_property
+    def graph_subcomponents(self):
+        return [
+            card for card in self.subcomponents
+            if hasattr(card,'graph')
+        ]
     
+
     @cached_property
     def content(self,params=None):
         cards=[
@@ -30,3 +37,30 @@ class FilterPanel(FilterComponent):
             self.log('subcomponent filters updated')
             self.filter_data = self.intersect_filters(*filters_d)
             return self.filter_data
+        
+
+
+
+class FilterPlotPanel(FilterPanel):
+    def component_callbacks(self, app):
+        super().component_callbacks(app)
+        @app.callback(
+            [
+                Output(card.graph,'figure',allow_duplicate=True)
+                for card in self.graph_subcomponents
+            ],
+            Input(self.store, 'data'),
+            prevent_initial_call=True
+        )
+        def redraw_graphs_from_new_data(panel_data):
+            filtered_keys = set(panel_data.get('intension',{}).keys())
+            return [
+                (
+                    dash.no_update 
+                    if card.key in filtered_keys 
+                    else card.plot(
+                        filter_data = panel_data, 
+                    )
+                )
+                for card in self.graph_subcomponents
+            ]
