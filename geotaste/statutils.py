2b2acdd7e241637ad776838f89927eba2343d79f
Ryan Heuser <ryan.heuser@gmail.com>
2023-07-12 04:27:36 -0400

    docs on statutils

--- geotaste/statutils.py
+++ geotaste/statutils.py
@@ -13,6 +13,23 @@ def analyze_contingency_tables(
         sort_asc=True, 
         min_p=MIN_P,
         signif=False):
+    """
+    Analyzes contingency tables created from two input value series.
+
+    Args:
+        vals1 (pd.Series): The first value series.
+        vals2 (pd.Series): The second value series.
+        funcs (list, optional): List of statistical functions to apply to each contingency table. Defaults to [odds_ratio, fisher_exact].
+        sort_by (str, optional): The column to sort the resulting DataFrame by. Defaults to 'odds_ratio'.
+        p_col (str, optional): The column containing p-values. Defaults to 'fisher_exact_p'.
+        sort_asc (bool, optional): Whether to sort the DataFrame in ascending order. Defaults to True.
+        min_p (float, optional): The minimum p-value threshold. Defaults to MIN_P.
+        signif (bool, optional): Whether to filter the DataFrame by p-value threshold. Defaults to False.
+
+    Returns:
+        pd.DataFrame: The resulting DataFrame containing the analyzed contingency tables.
+    """
+
     vals1=pd.Series(vals1)
     vals2=pd.Series(vals2)
     index_name='__'.join(sorted(list(set([vals1.name, vals2.name]))))
@@ -41,7 +58,8 @@ def analyze_contingency_tables(
     return df
 
 
-def iter_contingency_tables(vals1, vals2, iter_values=[]):
+def iter_contingency_tables(vals1, vals2, iter_values=[], min_count=None):
+
     assert is_listy(vals1),is_listy(vals2)
     s1,s2=pd.Series(vals1),pd.Series(vals2)
     counts1,counts2=s1.value_counts(), s2.value_counts()
@@ -53,9 +71,13 @@ def iter_contingency_tables(vals1, vals2, iter_values=[]):
         bl=int((counts1.sum() if len(counts1) else 0) - tl)
         br=int((counts2.sum() if len(counts2) else 0) - tr)
         return ((tl,tr),(bl,br))
+    
+    def total_count(tbl): return sum(tbl[0]) + sum(tbl[1])
 
     for uniqval in uniqvals:
-        yield (uniqval, get_contingency_table(uniqval))
+        tbl = get_contingency_table(uniqval)
+        if min_count is None or total_count(tbl) >= min_count:
+            yield (uniqval, tbl)
 
 
 
