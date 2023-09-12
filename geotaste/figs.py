e85fa6568379980c3a4fbfc4fd4d56a78622f3ce
Ryan Heuser <ryan.heuser@gmail.com>
2023-09-12 17:13:49 -0400

    tileserver-gl on custom server much faster

--- geotaste/figs.py
+++ geotaste/figs.py
@@ -548,19 +548,28 @@ def update_fig_mapbox_background(fig):
         # style='mapbox://styles/ryanheuser/cljef7th1000801qu6018gbx8',
         # style='stamen-toner',
         style="streets",
-        # layers=[
-        #     {
-        #         "below": 'traces',
-        #         "sourcetype": "raster",
-        #         "sourceattribution": "https://warper.wmflabs.org/maps/6050",
-        #         "source": [
-        #             "https://warper.wmflabs.org/maps/tile/6050/{z}/{x}/{y}.png"
-        #             # "/tiles/{z}/{x}/{y}.png"
-        #             # "http://127.0.0.1:5000/tiles/{z}/{x}/{y}.png"
-        #         ],
-        #         # "opacity":0.75
-        #     }
-        # ],
+        layers=[
+            # {
+            #     "below": 'traces',
+            #     "sourcetype": "raster",
+            #     "sourceattribution": "https://warper.wmflabs.org/maps/6050",
+            #     "source": [
+            #         "https://warper.wmflabs.org/maps/tile/6050/{z}/{x}/{y}.png"
+            #         # "/tiles/{z}/{x}/{y}.png"
+            #         # "http://127.0.0.1:5000/tiles/{z}/{x}/{y}.png"
+            #     ],
+            #     # "opacity":0.75
+            # }
+            {
+                "below": 'traces',
+                "sourcetype": "raster",
+                "sourceattribution": "paris1937",
+                "source": [
+                    "http://134.209.216.92:8080/data/paris1937/{z}/{x}/{y}.png"
+                ],
+                # "opacity":0.75
+            }
+        ],
         # style='mapbox://styles/ryanheuser/cllpenazf00ei01qi7c888uug',
         accesstoken=mapbox_access_token,
         bearing=0,
