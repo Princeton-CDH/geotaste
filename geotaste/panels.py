baaf6fe42185bc363e816f4f10322bcb1aae5d19
Ryan Heuser <ryan.heuser@gmail.com>
2023-09-16 08:22:03 -0400

    cleaning

--- geotaste/panels.py
+++ geotaste/panels.py
@@ -5,24 +5,8 @@ from .views import *
 class FilterPanel(FilterCard):
     unfiltered = UNFILTERED
 
-    # @cached_property
-    # def content(self,params=None):
-    #     return dbc.Container([self.store] + super().content.children)
-    
-    # # @cached_property
-    # # def store_desc(self): 
-    # #     return html.Span(
-    # #         self.unfiltered,
-    # #         id=self.id('query_str'), 
-    # #         className='store_desc query_str', 
-    # #         # placeholder=UNFILTERED, 
-    # #         # value='', 
-    # #         # style={'color':self.color}
-    # #     )
-
     def describe_filters(self, panel_data):
         return filter_query_str(panel_data, human=True)
-
 
     def component_callbacks(self, app):
         super().component_callbacks(app)
@@ -59,21 +43,14 @@ class FilterPanel(FilterCard):
                 ],
                 prevent_initial_call=True
             )
-            #@logger.catch
             def panel_filter_data_changed(panel_filter_data, *old_filter_data_l):
-                #???
                 if panel_filter_data is None: panel_filter_data={}
                 logger.trace(f'[{self.name}] updating my {len(self.store_panel_subcomponents)} subcomponents with my new panel filter data')
                 new_filter_data_l = [
-                    # {k:v for k,v in panel_filter_data.items() if k!=card.key}
                     panel_filter_data
                     for card in self.store_panel_subcomponents    
                 ]
                 out = new_filter_data_l
-                # out = [
-                #     new if new!=old else dash.no_update
-                #     for new,old in zip(new_filter_data_l, old_filter_data_l)
-                # ]
                 logger.debug(f'sending updates for new store_panel --> {out}')
                 return out
             
@@ -380,35 +357,6 @@ class ComparisonPanel(BaseComponent):
         className='graphtab padded', 
         id='maintbl'
     )
-    
-    # def get_mainview(self, fdL={}, fdR={}, viewfunc = MapView):
-    #     # get figure factory
-    #     num_filters = len([x for x in [fdL,fdR] if x])
-    #     # 3 cases
-    #     if num_filters==0:
-    #         ff = LandmarksFigureFactory()
-
-    #     elif num_filters==1:
-    #         if fdL:
-    #             ff = CombinedFigureFactory(fdL, color=LEFT_COLOR)
-    #         elif fdR:
-    #             ff = CombinedFigureFactory(fdR, color=RIGHT_COLOR)
-
-    #     elif num_filters == 2:
-    #         ff = ComparisonFigureFactory(fdL, fdR)
-
-    #     # return view
-    #     return dbc.Container(
-    #         dbc.Tabs(
-    #             [
-    #                 dbc.Tab(MapView(ff), label='Map', tab_id='map'),
-    #                 dbc.Tab(TableView(ff), label='Table', tab_id='table'),
-    #             ],
-    #             id='mainview_tabs',  
-    #             active_tab='map'
-    #         ),
-    #         className='viewfunc-container'
-    #     )
 
     def ff(self, fdL={}, fdR={}, **kwargs):
         # get figure factory
@@ -441,46 +389,21 @@ class ComparisonPanel(BaseComponent):
     def component_callbacks(self, app):
         super().component_callbacks(app)
 
-        # @app.callback(
-        #     Output('tblview-container','style',allow_duplicate=True),
-        #     [
-        #         Input(self.L.showhide_btn, "n_clicks"),
-        #         Input(self.L.header_btn, 'n_clicks'),
-        #         Input(self.R.showhide_btn, "n_clicks"),
-        #         Input(self.R.header_btn, 'n_clicks'),
-        #         Input(self.L.body, 'is_open'),
-        #         Input(self.R.body, 'is_open'),
-        #     ],
-        #     [
-        #         State('tblview-container','style'),
-        #     ],
-        #     prevent_initial_call=True
-        # )
-        # def resize_tbl_view(clk1,clk2,clk3,clk4, L_open,R_open, style_d):
-        #     logger.debug([clk1,clk2,clk3,clk4, L_open,R_open, style_d])
-        #     if style_d is None: style_d={}
-        #     style_d['padding-left']=f'{501 if R_open else (251 if L_open else 0)}px'
-        #     logger.debug(f'--> {style_d}')
-        #     return style_d
-        
-
-
-        ## CHANGING DATA
+        ### SWITCHING TABS
 
         @app.callback(
-            [
-                Output(self.maintbl,'children',allow_duplicate=True),
-                Output('layout-loading-output', 'children', allow_duplicate=True) # spinner
-            ],
-            Input(self.store,'data'),
-            # background=True,
-            # manager=background_manager,
+            Output('tblview','style',allow_duplicate=True),
+            Input(self.mainview_tabs,'active_tab'),
+            State('tblview','style'),
             prevent_initial_call=True
         )
-        def redo_tbl(data):
-            with Logwatch('running long callback computation'):
-                fdL,fdR=data
-                return get_server_cached_view(serialize([fdL,fdR,'table'])), True
+        def switch_tab_simple(active_tab, style_d):
+            if style_d is None: style_d={}
+            return {**style_d, **(STYLE_INVIS if active_tab=='map' else STYLE_VIS)}
+
+
+        ## CHANGING MAP
+        
 
         @app.callback(
             [
@@ -511,168 +434,26 @@ class ComparisonPanel(BaseComponent):
                 return ofig,otbl,[Lstore,Rstore],True
 
 
-            
-            
-        ### SWITCHING TABS
+
+        ## CHANGING DATA TABLE
 
         @app.callback(
-            Output('tblview','style',allow_duplicate=True),
-            Input(self.mainview_tabs,'active_tab'),
-            State('tblview','style'),
+            [
+                Output(self.maintbl,'children',allow_duplicate=True),
+                Output('layout-loading-output', 'children', allow_duplicate=True) # spinner
+            ],
+            Input(self.store,'data'),
+            # background=True,
+            # manager=background_manager,
             prevent_initial_call=True
         )
-        def switch_tab_simple(active_tab, style_d):
-            if style_d is None: style_d={}
-            return {**style_d, **(STYLE_INVIS if active_tab=='map' else STYLE_VIS)}
+        def redo_tbl(data):
+            with Logwatch('running long callback computation'):
+                fdL,fdR=data
+                return get_server_cached_view(serialize([fdL,fdR,'table'])), True
 
 
-        # @app.callback(
-        #     [
-        #         Output(self.mapview,'style',allow_duplicate=True),
-        #         Output(self.tblview,'style',allow_duplicate=True)
-        #     ],
-        #     [
-        #         Input(self.mainview_tabs, 'active_tab')
-        #     ],
-        #     [
-        #         State(self.mainmap,'figure'),
-        #         State(self.mapview,'style'),
-        #         State(self.tblview,'style'),
-        #         State(self.L.body, "is_open"),
-        #         State(self.R.body, 'is_open'),
-        #     ],
-        #     prevent_initial_call=True
-        # )
-        # def change_tab(active_tab, oldfig, imap_style, itbl_style, L_open, R_open):
-        #     if imap_style is None: imap_style={}
-        #     if itbl_style is None: itbl_style={}
-        #     logger.debug([active_tab, imap_style, itbl_style, L_open, R_open])
-        #     if active_tab=='map':
-        #         ofig_style = {**imap_style, **STYLE_VIS}
-        #         otbl_style = {**itbl_style, **STYLE_INVIS}
-        #     else:
-        #         ofig_style = {**imap_style, **STYLE_INVIS}
-        #         otbl_style = {**itbl_style, **STYLE_VIS}
-
-        #     otbl_style['left']=f'{500 if R_open else (250 if L_open else 0)}px'
-
-        #     out = ofig_style, otbl_style
-        #     logger.debug(f'--> {out}')
-        #     return out
-
-
-        # @app.callback(
-        #     [
-        #         Output(self.mainmap, 'figure',allow_duplicate=True),
-        #         Output(self.maintbl,'children',allow_duplicate=True),
-        #         Output(self.store_views, 'data',allow_duplicate=True)
-        #     ],
-        #     [
-        #         Input(self.L.store, 'data'),
-        #         Input(self.R.store, 'data'),
-        #     ],
-        #     [
-        #         State(self.mainmap,'figure'),
-        #         State(self.store_views, 'data')
-        #     ],
-        #     prevent_initial_call=True
-        # )
-        # def update_LR_data(Lstore, Rstore, oldfig, oldviews):
-        #     if oldviews is None: oldviews = {}
-        #     with Logwatch('returning informations'):
-        #         args_id=serialize([Lstore,Rstore])
-        #         if args_id in oldviews:
-        #             logger.debug(f'{args_id} found in oldviews!')
-        #             return oldviews[args_id] + [dash.no_update]
-                
-        #         else:
-        #             ofigdata,otbl = get_cached_views(args_id)
-        #             oldviews[args_id] = [ofigdata,otbl]
-        #             ofig = self.get_updated_fig(ofigdata,oldfig)
-        #             return [ofig,otbl,oldviews]
-
-
-
-    # def component_callbacks(self, app):
-    #     super().component_callbacks(app)
-
-    #     @app.callback(
-    #         [
-    #             Output(self.content_left, 'is_open', allow_duplicate=True),                      # dropdowns open
-    #             # Output(self.storedesc_R_col, 'style', allow_duplicate=True),                     # whether right filter button visible
-    #             Output(self.panel_R_col, 'style', allow_duplicate=True),                         # whether right filter panel visible
-    #             Output(self.graphtab, 'children', allow_duplicate=True),   # actual content
-    #             Output('layout-loading', 'children') # spinner
-    #         ],
-    #         [
-    #             Input(self.L.store, 'data'),                               # any changes in left filter
-    #             Input(self.R.store, 'data'),                               # any in right filter
-    #             Input(self.storedesc_L_btn, "n_clicks"),
-    #             Input(self.storedesc_R_btn, "n_clicks"),
-    #             Input(self.graphtabs, 'active_tab')
-    #         ],
-    #         [
-    #             State(self.content_left, 'is_open'),
-    #             State(self.L.store, 'data'),
-    #             State(self.R.store, 'data'),
-
-    #         ],
-    #         prevent_initial_call=True
-    #     )
-    #     def determine_dropdown_and_view(
-    #             fdL, 
-    #             fdR, 
-    #             storedesc_L_clicked, 
-    #             storedesc_R_clicked, 
-    #             active_tab, 
-    #             filter_dropdown_open_now,
-    #             old_fdL,
-    #             old_fdR,
-    #             ):
-    #         input_ids = [
-    #             self.L.store.id, 
-    #             self.R.store.id, 
-    #             self.storedesc_L_btn.id, 
-    #             self.storedesc_R_btn.id, 
-    #             self.graphtabs.id
-    #         ]
-    #         outs = [
-    #             dash.no_update,  # both dropdown vis
-    #             # dash.no_update,  # right vis
-    #             # dash.no_update,  # right vis
-    #             # STYLE_VIS if fdL and storedesc_R_clicked else STYLE_VIS,
-    #             # STYLE_VIS,
-    #             STYLE_VIS if fdL and storedesc_R_clicked else STYLE_INVIS,
-    #             dash.no_update   # content,
-    #         ]
             
-    #         logger.debug(ctx.triggered)
-    #         logger.debug(input_ids)
-
-    #         # if we clicked the Left or Right top Filter tab button
-    #         if ctx.triggered_id in {self.storedesc_L_btn.id, self.storedesc_R_btn.id}:
-    #             if storedesc_R_clicked!=1:
-    #                 outs[0]=not filter_dropdown_open_now
-
-    #         # if the tab changed -- or the filters changed
-    #         elif ctx.triggered_id in {self.graphtabs.id, self.L.store.id, self.R.store.id}:
-
-    #             # make sure change actually happened?
-    #             args = [[active_tab],[],fdL,fdR]
-    #             logger.debug(['switchtab'] + args)
-    #             logger.debug([fdL, old_fdL])
-    #             logger.debug([fdR, old_fdR])
-    #             serialized_data = serialize(args)
-    #             outs[-1] = graphtab_cache(serialized_data)
-
-    #             # also collapse dropdowns if tab changed
-    #             # if ctx.triggered_id == self.graphtabs.id:
-    #                 # outs[0] = False
-            
-    #         # logger.debug(outs)
-    #         return outs + [True]
-
-
 
         
             
