b83eafe8f9818d6a50ca88b722f9dc87b593a9de
Ryan Heuser <ryan.heuser@gmail.com>
2023-09-17 15:42:14 -0400

    test app working now?

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,16 @@ import sys,os
 sys.path.insert(0,os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from geotaste.app import get_app
 
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+
 def test_app_001(dash_duo):
     app = get_app()
     dash_duo.start_server(app)
