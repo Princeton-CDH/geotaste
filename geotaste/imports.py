b58eefbad66abfea451564355061178d8c8ae12d
Ryan Heuser <ryan.heuser@gmail.com>
2023-09-22 20:22:39 -0400

    all paths to main

--- geotaste/imports.py
+++ geotaste/imports.py
@@ -168,8 +168,8 @@ URLS=dict(
     events='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.3-beta/events.csv',
     landmarks='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/landmarks.csv',
     dwellings='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.3-beta/dwellings.csv',
-    creators='https://raw.githubusercontent.com/Princeton-CDH/geotaste/develop/data/1.3-beta/creators.csv',
-    combinedmini='https://raw.githubusercontent.com/Princeton-CDH/geotaste/develop/data/1.3-beta/combined.mini.pkl.gz',
+    creators='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.3-beta/creators.csv',
+    combinedmini='https://raw.githubusercontent.com/Princeton-CDH/geotaste/main/data/1.3-beta/combined.mini.pkl.gz',
     geojson_arrond='https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/arrondissements/exports/geojson?lang=en&timezone=Europe%2FParis'
 )
 
